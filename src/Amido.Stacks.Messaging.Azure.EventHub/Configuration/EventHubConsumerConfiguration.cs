7787b4d3df0b4b8ecb32a3b5be902b10d2727f83
Scott Vaughan <scott.vaughan@amido.com>
2021-08-27 09:59:44 +0100

    Create project solution for publisher. Create project solution for receiver.

---
+++ src/Amido.Stacks.Messaging.Azure.EventHub/Configuration/EventHubConsumerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Amido.Stacks.Messaging.Azure.EventHub.Configuration
+{
+    public class EventHubConsumerConfiguration
+    {
+        public string EventHubNamespaceConnectionString { get; set; }
+
+        public string EventHubName { get; set; }
+
+        public string BlobStorageConnectionString { get; set; }
+
+        public string BlobContainerName { get; set; }
+    }
+}
