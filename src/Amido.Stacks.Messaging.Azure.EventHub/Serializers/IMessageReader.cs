e7fb0f6de5ebadeb2208c936a6672ba5342009cb
scottvaughanamido <scott.vaughan@amido.com>
2021-09-14 09:17:50 +0100

    Add message reader to package

---
+++ src/Amido.Stacks.Messaging.Azure.EventHub/Serializers/IMessageReader.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Azure.EventHubs;
+
+namespace Amido.Stacks.Messaging.Azure.EventHub.Serializers
+{
+    public interface IMessageReader
+    {
+        T Read<T>(EventData eventData);
+    }
+}
